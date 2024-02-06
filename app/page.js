4abaff1166a3db9c1e089efa238ec8024ebcd53b
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-02-06 11:21:03 +0700

    Chapter 5 - Server Actions

--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
 import Button from "@/components/Button";
+import { getUser } from "@/lib/actions/action";
 
 export const metadata = {
   title: 'Home'
 };
 
-export default function Home() {
+export default async function Home() {
+  const users = await getUser();
   return (
     <div className="m-2">
       <Button />
+      <ul>
+      {
+        users.map(user => {
+          return <li>{user.name}</li>
+        })
+      }
+      </ul>
     </div>
   );
 }
