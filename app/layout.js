1508899adcf52d8b92f2e8be3001b6c916e163fe
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-02-07 16:35:33 +0700

    Chapter 8 - User Sign Up Process

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <Toaster position="bottom-right" />
         <Navbar />
         {children}
       </body>
