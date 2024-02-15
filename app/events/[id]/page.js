23c21ef7051e31f1183d31308f86ee9cface1864
Nathan Sebhastian <nathansebhastian@gmail.com>
2024-02-15 14:44:10 +0700

    Chapter 16: Displaying Events

--- app/events/[id]/page.js
+++ app/events/[id]/page.js
@@ -1,3 +1,41 @@
-export default function SingleEvent({ params }) {
-  return <h1>The event ID is: {params.id}</h1>;
+import {
+  getEventById,
+  getRelatedEventsByCategory,
+} from '@/lib/actions/event.action';
+import { getUserByEmail } from '@/lib/actions/user.action';
+import { authConfig } from '@/app/api/auth/[...nextauth]/authConfig';
+import { getServerSession } from 'next-auth';
+import EventDetail from '@/components/EventDetail';
+import EventCard from '@/components/EventCard';
+
+export default async function SingleEvent({ params: { id } }) {
+  const event = await getEventById(id);
+
+  let userId = '';
+  const data = await getServerSession(authConfig);
+  if (data?.user) {
+    const user = await getUserByEmail(data.user?.email);
+    userId = user._id;
+  }
+
+  const relatedEvents = await getRelatedEventsByCategory(
+    event.category,
+    event._id
+  );
+
+  return (
+    <>
+      <EventDetail event={event} userId={userId} />
+      {relatedEvents.length > 0 && (
+        <div className='p-6 pt-4 gap-6 mt-6 max-w-6xl'>
+          <h2 className='mb-4 text-3xl font-bold'>Related Events</h2>
+          <div className='grid lg:grid-cols-3 gap-4 mt-12'>
+            {relatedEvents.map(event => {
+              return <EventCard event={event} key={event._id} />;
+            })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
